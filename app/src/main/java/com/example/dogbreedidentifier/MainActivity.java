0336fada69f55677b93262533dad17d8059b52ff
Sandeep Tadepalli <sandeepteja1996@gmail.com>
2020-11-07 15:30:21 +0530

    Added Ads and updated strings

--- app/src/main/java/com/example/dogbreedidentifier/MainActivity.java
+++ app/src/main/java/com/example/dogbreedidentifier/MainActivity.java
@@ -31,6 +31,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -109,6 +115,17 @@ public class MainActivity extends AppCompatActivity {
 
             displayViewModelData();
         }
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+
+        AdView adView = findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder().build();
+        adView.loadAd(request);
     }
 
     @Override
