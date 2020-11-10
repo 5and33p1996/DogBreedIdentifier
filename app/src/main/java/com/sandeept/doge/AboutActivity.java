340eda21f808388f8e8034c9c190cb9659a6a5a6
Sandeep Tadepalli <sandeepteja1996@gmail.com>
2020-11-10 19:44:14 +0530

    Added privacy policy link in about page

--- app/src/main/java/com/sandeept/doge/AboutActivity.java
+++ app/src/main/java/com/sandeept/doge/AboutActivity.java
@@ -3,6 +3,8 @@ package com.sandeept.doge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView textView = findViewById(R.id.privacy_tnc);
+
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
